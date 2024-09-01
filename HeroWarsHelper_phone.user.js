2681f2fb8b15571ff688df724d98aaaa272bd41c
Thomas Gaud <36661892+Irmagon@users.noreply.github.com>
2024-09-01 18:32:48 +0300

    2.285

--- HeroWarsHelper_phone.user.js
+++ HeroWarsHelper_phone.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name			HWH_Phone
-// @name:en			HWH_Phone
-// @name:ru			HWH_Phone
+// @name			HeroWarsHelper
+// @name:en			HeroWarsHelper
+// @name:ru			HeroWarsHelper
 // @namespace		HeroWarsHelper
-// @version			2.284
+// @version			2.285
 // @description		Automation of actions for the game Hero Wars
 // @description:en	Automation of actions for the game Hero Wars
 // @description:ru	Автоматизация действий для игры Хроники Хаоса
@@ -2409,6 +2409,7 @@ async function checkChangeResponse(response) {
 			 */
 			if (call.ident == callsIdent['userGetInfo']) {
 				let user = call.result.response;
+				document.title = user.name;
 				userInfo = Object.assign({}, user);
 				delete userInfo.refillable;
 				if (!questsInfo['userGetInfo']) {
@@ -7841,6 +7842,7 @@ function rewardsAndMailFarm() {
 			const questGetAll = data.results[0].result.response.filter((e) => e.state == 2);
 			const questBattlePass = lib.getData('quest').battlePass;
 			const questChainBPass = lib.getData('battlePass').questChain;
+			const listBattlePass = lib.getData('battlePass').list;
 
 			const questAllFarmCall = {
 				calls: [],
@@ -7856,6 +7858,12 @@ function rewardsAndMailFarm() {
 					if (chain.requirement?.battlePassTicket) {
 						continue;
 					}
+					const battlePass = listBattlePass[chain.battlePass];
+					const startTime = battlePass.startCondition.time.value * 1e3
+					const endTime = new Date(startTime + battlePass.duration * 1e3); 
+					if (startTime > Date.now() || endTime < Date.now()) {
+						continue;
+				}
 				}
 				if (quest.id >= 2e7) {
 					questIds.push(quest.id);
