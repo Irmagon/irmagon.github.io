cd14fd786a580dbdf01370b6139c69e274eacee6
Thomas Gaud <36661892+Irmagon@users.noreply.github.com>
2024-10-04 07:36:05 +0300

    Update HeroWarsHelper_phone.user.js

--- HeroWarsHelper_phone.user.js
+++ HeroWarsHelper_phone.user.js
@@ -3,7 +3,7 @@
 // @name:en			HeroWarsHelper
 // @name:ru			HeroWarsHelper
 // @namespace		HeroWarsHelper
-// @version			2.288
+// @version			2.289
 // @description		Automation of actions for the game Hero Wars
 // @description:en	Automation of actions for the game Hero Wars
 // @description:ru	Автоматизация действий для игры Хроники Хаоса
